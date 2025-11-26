a566229a6170d34db4b4faf4b4d5b93966372a19
sditnurulkautsarbalbod-arch <sditnurulkautsarbalbod@gmail.com>
2025-11-26 08:31:28 +0800

    Add files via upload

---
+++ staffData.ts
@@ -0,0 +1,190 @@
+import { StaffType } from './types';
+
+export const staffData = [
+  {
+    "id": "hm-1",
+    "name": "Kasman, S.Pd.I",
+    "type": StaffType.HEADMASTER,
+    "position": "Kepala Sekolah"
+  },
+  {
+    "id": "tc-1",
+    "name": "Indah Sari, S.Pd., M.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 1A"
+  },
+  {
+    "id": "tc-2",
+    "name": "Rosnina Ruslan, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Pendamping Kelas 1A"
+  },
+  {
+    "id": "tc-3",
+    "name": "Vivi Angraeni, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 1B"
+  },
+  {
+    "id": "tc-4",
+    "name": "Wahdania, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Pendamping Kelas 1B"
+  },
+  {
+    "id": "tc-5",
+    "name": "Nur Fitriani, S.Pd.I.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 1C"
+  },
+  {
+    "id": "tc-6",
+    "name": "Anriana, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Pendamping Kelas 1C"
+  },
+  {
+    "id": "tc-7",
+    "name": "Nur Asura, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 1D"
+  },
+  {
+    "id": "tc-8",
+    "name": "Rifka Mulyani Jamil, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Pendamping Kelas 1D"
+  },
+  {
+    "id": "tc-9",
+    "name": "Muhammad Rate, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 2A"
+  },
+  {
+    "id": "tc-10",
+    "name": "A. Fifin Kamriani, S.P.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 2B"
+  },
+  {
+    "id": "tc-11",
+    "name": "Zahra Karimah Tunnisa, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 2C"
+  },
+  {
+    "id": "tc-12",
+    "name": "Nur Ariani Amir, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 2D"
+  },
+  {
+    "id": "tc-13",
+    "name": "Siswandi Zikrullah",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 3A"
+  },
+  {
+    "id": "tc-14",
+    "name": "La Ode Hermawan Rauf, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Pendamping Kelas 3A"
+  },
+  {
+    "id": "tc-15",
+    "name": "Hasanuddin, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 3B"
+  },
+  {
+    "id": "tc-16",
+    "name": "Haaniyah Bintu Kasman, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 3C"
+  },
+  {
+    "id": "tc-17",
+    "name": "Darmawati Saleh, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 3D"
+  },
+  {
+    "id": "tc-18",
+    "name": "M. Alimin, S.Pd.I.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 4A"
+  },
+  {
+    "id": "tc-19",
+    "name": "Soekarno, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 4B"
+  },
+  {
+    "id": "tc-20",
+    "name": "Sumarni, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 4C"
+  },
+  {
+    "id": "tc-21",
+    "name": "Nurfatia, S.Hum.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 4D"
+  },
+  {
+    "id": "tc-22",
+    "name": "Muhammad Asrar Idrus, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 5A"
+  },
+  {
+    "id": "tc-23",
+    "name": "Sri Nikmayanti, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 5B"
+  },
+  {
+    "id": "tc-24",
+    "name": "Muhammad Rusdi Syam, S.E.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 6A"
+  },
+  {
+    "id": "tc-25",
+    "name": "Nur Fadillah Musfira, S.Pd.",
+    "type": StaffType.TEACHER,
+    "position": "Wali Kelas 6B"
+  },
+  {
+    "id": "st-1",
+    "name": "M. Anshar Gaffar, S.Sos.",
+    "type": StaffType.STAFF,
+    "position": "OPS / Admin"
+  },
+  {
+    "id": "st-2",
+    "name": "Alwi Firsadi, S.Kom.",
+    "type": StaffType.STAFF,
+    "position": "Bendahara"
+  },
+  {
+    "id": "st-3",
+    "name": "Muh. Saldi",
+    "type": StaffType.STAFF,
+    "position": "Keamanan"
+  },
+  {
+    "id": "st-4",
+    "name": "Malik",
+    "type": StaffType.STAFF,
+    "position": "Keamanan"
+  },
+  {
+    "id": "st-5",
+    "name": "Hastuti",
+    "type": StaffType.STAFF,
+    "position": "Kebersihan"
+  }
+];
